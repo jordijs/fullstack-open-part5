7cd637b4df72b492d0e4c4fb135f81fcc9941f4d
Jordi <jordijs6@gmail.com>
2024-07-14 22:40:24 +0200

    4.8 - test if returned blogs number is correct

--- bloglist/tests/blogs_api.test.js
+++ bloglist/tests/blogs_api.test.js
@@ -1,30 +1,31 @@
-const { test, after, beforeEach, describe,  } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
-// const blogsHelper = require('../utils/blogs_helper')
+const { initialBlogs } = require('./blogs_api_helper')
 
 const api = supertest(app)
 
 describe('blogs api', () => {
 
-  // beforeEach(async () => {
-  //   await Note.deleteMany({})
-  //   for (let note of helper.initialNotes) {
-  //     let noteObject = new Note(note)
-  //     await noteObject.save()
-  //   }
-  // })
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    for (let blog of initialBlogs) {
+      let blogObject = new Blog(blog)
+      await blogObject.save()
+    }
+  })
 
-  test('blogs are returned as json', async () => {
-    await api
+  test('existing number of blogs are returned as json', async () => {
+    const response = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+    assert.strictEqual(response.body.length, initialBlogs.length)
   })
-
 
   after(async () => {
     await mongoose.connection.close()
