bafe458547fb7626ce20f1ab8aec38076cc1fe81
Jordi <jordijs6@gmail.com>
2024-07-14 22:07:11 +0200

    4.8 - add: close mongoose connection

--- bloglist/tests/blogs_api.test.js
+++ bloglist/tests/blogs_api.test.js
@@ -1,21 +1,33 @@
-const { test, describe, after } = require('node:test')
+const { test, after, beforeEach, describe,  } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
-// const listHelper = require('../utils/list_helper')
+// const blogsHelper = require('../utils/blogs_helper')
 
 const api = supertest(app)
 
-
 describe('blogs api', () => {
+
+  // beforeEach(async () => {
+  //   await Note.deleteMany({})
+  //   for (let note of helper.initialNotes) {
+  //     let noteObject = new Note(note)
+  //     await noteObject.save()
+  //   }
+  // })
 
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+  })
+
+
+  after(async () => {
+    await mongoose.connection.close()
   })
 
 })
