c170e1d1e2b1fc6a8d025207e755cbbc766674ff
Jordi <jordijs6@gmail.com>
2024-07-24 20:28:02 +0200

    4.20*: Blog List Expansion, step 8

--- bloglist/utils/middleware.js
+++ bloglist/utils/middleware.js
@@ -13,7 +13,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return response.status(400).json({ error: 'expected `username` to be unique' })
-  } else if (error.name ===  'JsonWebTokenError') {
+  } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'token invalid' })
   }
 
@@ -22,12 +22,11 @@ const errorHandler = (error, request, response, next) => {
 
 const tokenExtractor = (request, response, next) => {
 
-  const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.startsWith('Bearer ')) {
-      return authorization.replace('Bearer ', '')
-    }
-    return null
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    const token = authorization.replace('Bearer ', '')
+    request.token = token
   }
 
   next()
