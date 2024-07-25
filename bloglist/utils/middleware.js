76f1333a2007191ed740c3ee29fa8bead6fcc03a
Jordi <jordijs6@gmail.com>
2024-07-25 22:45:51 +0200

    4.22 - implement userExtractor on blogs POST

--- bloglist/utils/middleware.js
+++ bloglist/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -32,7 +34,13 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
-const userExtractor = (request, response, next) => {
+const userExtractor = async (request, response, next) => {
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  request.user = await User.findById(decodedToken.id)
 
   next()
 }
