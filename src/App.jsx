325d59e7797eb82edf155e0c4c93ff2e6d53dc8f
Jordi <jordijs6@gmail.com>
2024-08-01 14:16:26 +0200

    5.1 - complete login functionality and conditional render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,44 +15,55 @@ const App = () => {
     )
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error(exception)
+    }
   }
 
-  // if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
+  if (user === null) {
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
         <div>
           username
-            <input
+          <input
             type="text"
             value={username}
             name="Username"
+            autoComplete="username"
             onChange={({ target }) => setUsername(target.value)}
           />
         </div>
         <div>
           password
-            <input
+          <input
             type="password"
             value={password}
             name="Password"
+            autoComplete="current-password"
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
         <button type="submit">login</button>
-        </form>
-      </div>
-    )
-  // }
+      </form>
+    </div>
+  )
+  }
 
   return (
     <div>
-      <p>{{username}} logged in</p>
       <h2>blogs</h2>
+      <p>{ user.name } logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
