dff217d3ffeabf9310a8d8b4de1ab774e627c784
Jordi <jordijs6@gmail.com>
2024-08-01 16:28:57 +0200

    5.2 - implement logout

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,14 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem(
+      'loggedBloglistUser', JSON.stringify(user)
+    )
+    setUser(null)
+    
+  }
+
   if (user === null) {
   return (
     <div>
@@ -75,7 +83,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{ user.name } logged in</p>
+      <p>{ user.name } logged in<button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
