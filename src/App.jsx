d6df7a4d41159c7eb198fa2ed97400e15db29337
Jordi <jordijs6@gmail.com>
2024-08-05 21:27:31 +0200

    5.5 - implement Togglable component, applies on click to button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import Notification from "./components/Notification"
 import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -102,11 +103,14 @@ const App = () => {
       <h2>blogs</h2>
       <Notification notification={notification} />
       <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
-      <BlogForm
-        handleBlogForm={handleBlogForm}
-        newBlog={newBlog}
-        setNewBlog={setNewBlog}
-      />
+      <Togglable buttonLabel="new blog">
+        <BlogForm
+          handleBlogForm={handleBlogForm}
+          newBlog={newBlog}
+          setNewBlog={setNewBlog}
+        />
+      </Togglable>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
